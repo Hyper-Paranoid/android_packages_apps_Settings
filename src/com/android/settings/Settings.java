de964f15f4fe89f3bb9575654d7f162ec319a66f
spezi77 <spezi7713@gmx.net>
2019-12-27 14:28:57 +0100

    Add Lockscreen display activity to use as shortcut in the QS tile.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -235,6 +235,8 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    public static class LockscreenDisplayActivity extends SettingsActivity { /* empty */ }
+
     /**
      * Activity for MediaControlsSettings
      */
