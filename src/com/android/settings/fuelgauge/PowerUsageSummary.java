Evolution-X-AOSP/packages_apps_Settings

Settings: Categorize battery settings
Change-Id: Iaa699332a15079b2eb8f5b4b1b6a57ce0290ab99
Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -37,6 +37,7 @@ import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
@@ -81,6 +82,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
+    private static final String KEY_SMART_CHARGING_CATEGORY = "smart_charging_category";
+    private static final String KEY_BATTERY_INFO_CATEGORY = "battery_info_category";
     private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
     private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
     private static final String KEY_BATTERY_CHARGE_CYCLES = "battery_charge_cycles";
@@ -89,6 +92,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private String mBatCurCap;
     private String mBatChgCyc;
 
+    private PreferenceCategory mSmartChargingCat;
+    private PreferenceCategory mBatteryInfoCat;
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
     @VisibleForTesting
@@ -256,24 +261,15 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mBatteryTemp = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
+        mBatteryInfoCat = (PreferenceCategory) findPreference(KEY_BATTERY_INFO_CATEGORY);
+
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
  
         // Check availability of Battery Health
-        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
         if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mDesignedHealthPref);
-        }
-
-        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCurrentHealthPref);
-        }
-
-        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCyclesHealthPref);
+            getPreferenceScreen().removePreference(mBatteryInfoCat);
         }
     }
 

Hyper-Paranoid/android_packages_apps_Settings

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -37,6 +37,7 @@ import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
@@ -81,6 +82,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
+    private static final String KEY_SMART_CHARGING_CATEGORY = "smart_charging_category";
+    private static final String KEY_BATTERY_HEALTH_CATEGORY = "battery_health_category";
     private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
     private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
     private static final String KEY_BATTERY_CHARGE_CYCLES = "battery_charge_cycles";
@@ -89,6 +92,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private String mBatCurCap;
     private String mBatChgCyc;
 
+    private PreferenceCategory mSmartChargingCat;
+    private PreferenceCategory mBatteryHealthCat;
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
     @VisibleForTesting
@@ -256,24 +261,15 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mBatteryTemp = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
+        mBatteryHealthCat = (PreferenceCategory) findPreference(KEY_BATTERY_HEALTH_CATEGORY);
+
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
  
         // Check availability of Battery Health
-        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
         if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mDesignedHealthPref);
-        }
-
-        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCurrentHealthPref);
-        }
-
-        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCyclesHealthPref);
+            getPreferenceScreen().removePreference(mBatteryHealthCat);
         }
     }
 